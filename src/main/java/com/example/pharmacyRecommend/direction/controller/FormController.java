e330edc5a137b6d74d35d8c02362dc9bf611c0c5
LeeJaeHoon43 <ljw160791@gmail.com>
2023-07-29 21:24:59 +0900

    * Handlebars를 이용한 화면 만들기 - MockMVC를 이용한 테스트 케이스 작성.

--- src/main/java/com/example/pharmacyRecommend/direction/controller/FormController.java
+++ src/main/java/com/example/pharmacyRecommend/direction/controller/FormController.java
@@ -1,9 +1,13 @@
 package com.example.pharmacyRecommend.direction.controller;
 
+import com.example.pharmacyRecommend.direction.dto.InputDto;
 import com.example.pharmacyRecommend.pharmacy.service.PharmacyRecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,5 +18,13 @@ public class FormController {
     @GetMapping("/")
     public String main(){
         return "main";
+    }
+
+    @PostMapping("/search")
+    public ModelAndView postDirection(@ModelAttribute InputDto inputDto){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("output");
+        modelAndView.addObject("outputFormList", pharmacyRecommendationService.recommendPharmacyList(inputDto.getAddress()));
+        return modelAndView;
     }
 }
--- src/main/java/com/example/pharmacyRecommend/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/pharmacyRecommend/pharmacy/service/PharmacyRecommendationService.java
@@ -3,14 +3,17 @@ package com.example.pharmacyRecommend.pharmacy.service;
 import com.example.pharmacyRecommend.api.dto.DocumentDto;
 import com.example.pharmacyRecommend.api.dto.KakaoApiResponseDto;
 import com.example.pharmacyRecommend.api.service.KakaoAddressSearchService;
+import com.example.pharmacyRecommend.direction.dto.OutputDto;
 import com.example.pharmacyRecommend.direction.entity.Direction;
 import com.example.pharmacyRecommend.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -22,17 +25,34 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService.recommendPharmacyList fail] Input address: {}", address);
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
+        // 카카오 카테고리를 이용한 장소 검색 API 이용
         // List<Direction> directionList = directionService.buildDirectionList(documentDto);
+
+        // 공공기관 약국 데이터 및 거리 계산 알고리즘 이용
         List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
-        directionService.saveAll(directionList);
+
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
+    }
+    
+    private OutputDto convertToOutputDto(Direction direction){
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl("todo") // TODO
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
